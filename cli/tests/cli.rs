c9abfe06cf36ca2272304c184d33cdad2df1d657
Efi MK <efi.merdler@gmail.com>
2023-07-01 19:22:59 +0000

    wip

--- cli/tests/cli.rs
+++ cli/tests/cli.rs
@@ -2,20 +2,7 @@
 mod cli_tests {
     use assert_cmd::prelude::*;
     use predicates::prelude::*;
-    use rand::Rng;
     use std::process::Command;
-
-    #[cfg(feature = "aws_configured")]
-    #[test]
-    fn wrong_profile() {
-        let mut rng = rand::thread_rng();
-        let mut cmd = Command::cargo_bin("whoami").unwrap();
-
-        cmd.arg("--profile").arg(format!("{}", rng.gen::<u8>()));
-        cmd.assert().failure().stderr(predicate::str::contains(
-            "Failed loading AWS config details",
-        ));
-    }
 
     #[cfg(feature = "aws_configured")]
     #[test]
